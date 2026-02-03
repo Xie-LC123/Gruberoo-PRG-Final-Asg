f8a8c41c96fe5a67297b67f0da45bef1519bc2fc
shengle-ngeeann <s10273654@connect.np.edu.sg>
2026-02-03 18:43:10 +0800

    the  7 classes

---
+++ OrderedFoodItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruberoo
+{
+    internal class OrderedFoodItem : FoodItem
+    {
+        public OrderedFoodItem(string itemName, string itemDesc, double itemPrice, string customise) : base(itemName, itemDesc, itemPrice, customise)
+        {
+        }
+
+        public int QtyOrdered { get; set; }
+        public double SubTotal { get; private set; }
+
+        public double CalculateSubtotal()
+        {
+            SubTotal = ItemPrice * QtyOrdered;
+            return SubTotal;
+        }
+
+    }
+}
+
---
+++ SpecialOffer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruberoo
+{
+    internal class SpecialOffer
+    {
+        public string OfferCode { get; set; }
+        public string OfferDesc { get; set; }
+        public double Discount { get; set; }
+
+        public SpecialOffer(string offerCode, string offerDesc, double discount)
+        {
+            OfferCode=offerCode;
+            OfferDesc=offerDesc;
+            Discount=discount;
+        }
+        public override string ToString()
+        {
+            return $"{OfferCode}: {OfferDesc} (-${Discount})";
+        }
+    }
+}
