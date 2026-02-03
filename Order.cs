c2b403d7b27f2dcc8cfcee6efba59d329816e9ad
shengle-ngeeann <s10273654@connect.np.edu.sg>
2026-02-03 18:38:18 +0800

    The 7 classes

---
+++ Order.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruberoo
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public double OrderTotal { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime DeliverDateTime { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string OrderPaymentMethod { get; set; }
+        public bool OrderPaid { get; set; }
+
+        public Order(int orderId, DateTime orderDateTime, double orderTotal, string orderStatus, DateTime deliverDateTime, string deliveryAddress, string orderPaymentMethod, bool orderPaid)
+        {
+            OrderId = orderId;
+            OrderDateTime = orderDateTime;
+            OrderTotal = orderTotal;
+            OrderStatus = orderStatus;
+            DeliverDateTime = deliverDateTime;
+            DeliveryAddress = deliveryAddress;
+            OrderPaymentMethod = orderPaymentMethod;
+            OrderPaid = orderPaid;
+        }
+        private List<OrderedFoodItem> orderedFoodItems = new List<OrderedFoodItem>();
+        private List<SpecialOffer> specialOffers = new List<SpecialOffer>();
+
+        public double CalculateOrderTotal()
+        {
+            double total = 0;
+            foreach (var item in orderedFoodItems)
+            {
+                total += item.CalculateSubtotal();
+            }
+
+            foreach (var offer in specialOffers)
+            {
+                total -= offer.Discount;
+            }
+
+            OrderTotal = total;
+            return OrderTotal;
+        }
+        public void AddOrderedFoodItem(OrderedFoodItem item)
+        {
+            orderedFoodItems.Add(item);
+        }   
+
+    }
+}
---
+++ Restaurant.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruberoo
+{
+    internal class Restaurant
+    {
+        public string RestaurantId { get; set; }
+        public string RestaurantName { get; set; }
+        public string RestaurantStatus { get; set; }
+
+        public Restaurant(string restaurantId, string restaurantName, string restaurantStatus)
+        {
+            RestaurantId=restaurantId;
+            RestaurantName=restaurantName;
+            RestaurantStatus=restaurantStatus;
+        }
+        private List<Menu> menus = new List<Menu>();
+        private List<Order> orders = new List<Order>();
+        private List<SpecialOffer> specialOffers = new List<SpecialOffer>();
+
+        public void AddMenu(Menu menu)
+        {
+            menus.Add(menu);
+        }
+
+        public bool RemoveMenu(Menu menu)
+        {
+            return menus.Remove(menu);
+        }
+
+        public void DisplayMenu()
+        {
+            foreach (var menu in menus)
+            {
+                System.Console.WriteLine(menu);
+            }
+        }
+
+        public void DisplayOrders()
+        {
+            foreach (var order in orders)
+            {
+                System.Console.WriteLine(order);
+            }
+        }
+
+        public void DisplaySpecialOffers()
+        {
+            foreach (var offer in specialOffers)
+            {
+                System.Console.WriteLine(offer);
+            }
+        }
+
+        public override string ToString()
+        {
+            return RestaurantName;
+        }
+
+    }
+}
